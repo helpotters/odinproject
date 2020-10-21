58108b7239db3935d23fe0601a98f213b540d88f
Paul Lemus <paul.lemus1997@gmail.com>
2020-10-21 16:12:18 -0700

    Comments added to caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -12,12 +12,12 @@ def caesar_cipher(input, shift = 0)
   p output
 end
 
-encrypted_text = caesar_cipher("Test, 123",100)
+encrypted_text = caesar_cipher("Test, 123",10) # "^o}~6*;<="
 
 # Decrypted Text
-caesar_cipher(encrypted_text,-100)
+caesar_cipher(encrypted_text,-10)
 
-encrypted_numbers =  caesar_cipher(1234,23)
+encrypted_numbers =  caesar_cipher(1234,23) # "HIJK"
 
 #Decrypted Numbers
 caesar_cipher(encrypted_numbers,-23)
