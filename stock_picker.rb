4e912f9173a2d2b7488a3c1abff037170ae36b59
Paul Lemus <paul.lemus1997@gmail.com>
2020-10-23 12:49:53 -0700

    Turnips.

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,9 +2,9 @@
 # Stock Picker
 turnip_prices = [601,90,60,60,100,120,240,600,90,60,40,600,50,40]
 
-def biggest_difference(prices)
+def turnip_guide(prices)
 
-  stocks = {
+  turnps = {
     buy_day: [],
     sell_day: [],
     difference: [],
@@ -16,20 +16,20 @@ def biggest_difference(prices)
       if difference <= 0
         next
       else
-        stocks[:buy_day].push(prices.index(price))
-        stocks[:sell_day].push(prices.index(prices[day + i]))
-        stocks[:difference].push(difference)
+        turnps[:buy_day].push(prices.index(price))
+        turnps[:sell_day].push(prices.index(prices[day + i]))
+        turnps[:difference].push(difference)
       end
     end
   }
 
-  best_profit = stocks[:difference].to_a.max
-  date = stocks[:difference].index(best_profit)
+  best_profit = turnps[:difference].to_a.max
+  date = turnps[:difference].index(best_profit)
 
-  p "Buy the #{stocks[:buy_day][date]} and \
-  sell on the #{stocks[:sell_day][date]} for a \
-  profit of #{stocks[:difference][date]}"
+  p "Buy the #{turnps[:buy_day][date]} and \
+  sell on the #{turnps[:sell_day][date]} for a \
+  profit of #{turnps[:difference][date]} bells."
 
 end
 
-biggest_difference(turnip_prices)
+turnip_guide(turnip_prices)
